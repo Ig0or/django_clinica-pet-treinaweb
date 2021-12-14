65b4e1750a51e77a88ba21f40ff3013b198b3df2
Ig0or <igorprovensi@gmail.com>
2021-12-14 16:03:02 -0300

    inicio models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from localflavor.br.br_states import STATE_CHOICES
 
-# Create your models here.
+class EnderecoCliente(models.Model):
+    rua = models.CharField(max_length=50, null=False, blank=False)
+    numero = models.CharField(max_length=10, null=False, blank=False)
+    complemento = models.CharField(max_length=50, null=True, blank=True)
+    cidade = models.CharField(max_length=50, null=False, blank=False)
+    estado = models.CharField(max_length=2, choices=STATE_CHOICES, null=False, blank=False)
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+# telefone
+    endereco = models.ForeignKey(EnderecoCliente, on_delete=CASCADE)
