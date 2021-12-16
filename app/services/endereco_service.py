2dcc68da80cc7ac803c2d8d5b8623f6f9ae74254
Ig0or <igorprovensi@gmail.com>
2021-12-16 10:41:10 -0300

    metodo editar cliente finalizado

--- app/services/endereco_service.py
+++ app/services/endereco_service.py
@@ -1,4 +1,16 @@
 from ..models import EnderecoCliente
 
 def cadastrar_endereco(endereco):
-    return EnderecoCliente.objects.create(rua=endereco.rua, numero=endereco.rua, complemento=endereco.complemento, cidade=endereco.cidade, estado=endereco.estado)
+    return EnderecoCliente.objects.create(rua=endereco.rua, numero=endereco.numero, complemento=endereco.complemento, cidade=endereco.cidade, estado=endereco.estado)
+
+def listar_endereco_id(id):
+    return EnderecoCliente.objects.get(id=id)
+
+def editar_endereco(endereco_antigo, endereco_novo):
+    endereco_antigo.rua = endereco_novo.rua
+    endereco_antigo.numero = endereco_novo.numero
+    endereco_antigo.complemento = endereco_novo.complemento
+    endereco_antigo.cidade = endereco_novo.cidade
+    endereco_antigo.estado = endereco_novo.estado
+    endereco_antigo.save(force_update=True)
+    return endereco_antigo
